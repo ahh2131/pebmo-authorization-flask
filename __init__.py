bdc0691602fa5fb12c488e159799e53a538c3dbc
Andy <ahh2131@columbia.edu>
2014-02-25 16:37:45 -0500

    Added post request to venmo api to recieve access token

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask import Flask
 from flask import request
+import requests
 
 app = Flask(__name__)
 
@@ -12,8 +13,16 @@ def hello_world():
 
 @app.route('/', methods = ['GET'])
 def send_token():
-    access_token = request.args.get('access_token')
-    return render_template('index.html', access_token=access_token)
+    code = request.args.get('code')
+    data = {
+        "client_id":"1615",
+        "client_secret":"SXHt7emkMzT36wmdLs96sgasMCqw5TAT",
+        "code":code
+        }
+    url = "https://api.venmo.com/v1/oauth/access_token"
+    response = requests.post(url, data)
+    response_dict = response.json()
+    return render_template('index.html', access_token=response_dict['access_token'])
 
 if __name__ == '__main__':
     app.run(debug=True)
