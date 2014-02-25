960a68bd0f2c77244e2c06bf955ec13774688b0d
Andy <ahh2131@columbia.edu>
2014-02-25 17:06:22 -0500

    Fix acccess code appearance

--- __init__.py
+++ __init__.py
@@ -22,6 +22,7 @@ def send_token():
     url = "https://api.venmo.com/v1/oauth/access_token"
     response = requests.post(url, data)
     response_dict = response.json()
+    print(response_dict)
     return render_template('index.html', access_token=response_dict["access_token"])
 
 if __name__ == '__main__':
